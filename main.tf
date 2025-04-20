4b2f79f43bddf771e0f4d36164d88a0b469a538c
BruhSlave <braslpaf@gmail.com>
2025-04-20 12:45:32 +0000

    Add repeated_zones for duplicate instances and ssh metadata

--- main.tf
+++ main.tf
@@ -3,60 +3,40 @@ locals {
   boot_disk_name      = var.boot_disk_name != null ? var.boot_disk_name : "${var.name_prefix}-boot-disk"
   linux_vm_name       = var.linux_vm_name != null ? var.linux_vm_name : "${var.name_prefix}-linux-vm"
   vpc_network_name    = var.vpc_network_name != null ? var.vpc_network_name : "${var.name_prefix}-private"
-  ydb_serverless_name = var.ydb_serverless_name != null ? var.ydb_serverless_name : "${var.name_prefix}-ydb-serverless"
   bucket_sa_name      = var.bucket_sa_name != null ? var.bucket_sa_name : "${var.name_prefix}-bucket-sa"
   bucket_name         = var.bucket_name != null ? var.bucket_name : "${var.name_prefix}-terraform-bucket-${random_string.bucket_name.result}"
+  repeated_zones = flatten([
+    for zone in var.zones : [
+      for i in range(var.vm_count_per_zone) : {
+        zone  = zone
+        index = i
+        key   = "${zone}-${i}"
+      }
+    ]
+  ])
+  vm_map = { for z in local.repeated_zones : z.key => z }
 }
 
 # Создание дисков и виртуальных машин
 resource "yandex_compute_disk" "boot_disk" {
-  for_each = var.zones
+  for_each = local.vm_map
 
-  name     = length(var.zones) > 1 ? "${local.boot_disk_name}-${substr(each.value, -1, 0)}" : local.boot_disk_name
-  zone     = each.value
+  name     = "${local.boot_disk_name}-${substr(each.value.zone, -1, 0)}-${each.value.index}"
+  zone     = each.value.zone
   image_id = var.image_id
 
   type = var.instance_resources.disk.disk_type
   size = var.instance_resources.disk.disk_size
 }
 
-resource "yandex_compute_disk" "secondary_disk_a" {
-  count = contains(var.zones, "ru-central1-a") ? var.secondary_disks.count : 0
-
-  name = "${var.secondary_disks.name}-a-${count.index}"
-  zone = "ru-central1-a"
-
-  type = var.secondary_disks.type
-  size = var.secondary_disks.size
-}
-
-resource "yandex_compute_disk" "secondary_disk_b" {
-  count = contains(var.zones, "ru-central1-b") ? var.secondary_disks.count : 0
-
-  name = "${var.secondary_disks.name}-b-${count.index}"
-  zone = "ru-central1-b"
-
-  type = var.secondary_disks.type
-  size = var.secondary_disks.size
-}
-
-resource "yandex_compute_disk" "secondary_disk_d" {
-  count = contains(var.zones, "ru-central1-d") ? var.secondary_disks.count : 0
-
-  name = "${var.secondary_disks.name}-d-${count.index}"
-  zone = "ru-central1-d"
-
-  type = var.secondary_disks.type
-  size = var.secondary_disks.size
-}
-
 resource "yandex_compute_instance" "this" {
-  for_each = var.zones
+  for_each = local.vm_map
 
-  name                      = length(var.zones) > 1 ? "${local.linux_vm_name}-${substr(each.value, -1, 0)}" : local.linux_vm_name
+  name = "${local.linux_vm_name}-${substr(each.value.zone, -1, 0)}-${each.value.index}"
+
   allow_stopping_for_update = true
   platform_id               = var.instance_resources.platform_id
-  zone                      = each.value
+  zone                      = each.value.zone
 
   resources {
     cores  = var.instance_resources.cores
@@ -64,22 +44,22 @@ resource "yandex_compute_instance" "this" {
   }
 
   boot_disk {
-    disk_id = yandex_compute_disk.boot_disk[each.value].id
+    disk_id = yandex_compute_disk.boot_disk[each.key].id
   }
 
-  dynamic "secondary_disk" {
-    for_each = each.value == "ru-central1-a" ? yandex_compute_disk.secondary_disk_a : each.value == "ru-central1-b" ? yandex_compute_disk.secondary_disk_b : each.value == "ru-central1-d" ? yandex_compute_disk.secondary_disk_d : []
-    content {
-      disk_id = try(secondary_disk.value.id, null)
-    }
+  metadata = {
+    ssh-keys = "ubuntu:${var.ssh_public_key}"
   }
 
   network_interface {
-    subnet_id      = yandex_vpc_subnet.private[each.value].id
-    nat            = true
-    nat_ip_address = yandex_vpc_address.this[each.value].external_ipv4_address[0].address
+    subnet_id = yandex_vpc_subnet.private[each.value.zone].id
+    nat       = each.value.index == 0 ? true : false
   }
+
+  # Создание дисков до ВМ
+  depends_on = [yandex_compute_disk.boot_disk]
 }
+
 
 # Создание VPC и подсети
 resource "yandex_vpc_network" "this" {
@@ -99,15 +79,10 @@ resource "yandex_vpc_address" "this" {
   for_each = var.zones
 
   name = length(var.zones) > 1 ? "${local.linux_vm_name}-address-${substr(each.value, -1, 0)}" : "${local.linux_vm_name}-address"
- 
+
   external_ipv4_address {
     zone_id = each.value
   }
-}
-
-# Создание Yandex Managed Service for YDB
-resource "yandex_ydb_database_serverless" "this" {
-  name = local.ydb_serverless_name
 }
 
 # Создание сервисного аккаунта 
@@ -133,8 +108,8 @@ resource "yandex_storage_bucket" "this" {
   bucket     = local.bucket_name
   access_key = yandex_iam_service_account_static_access_key.this.access_key
   secret_key = yandex_iam_service_account_static_access_key.this.secret_key
-  
-  depends_on = [ yandex_resourcemanager_folder_iam_member.storage_editor ]
+
+  depends_on = [yandex_resourcemanager_folder_iam_member.storage_editor]
 }
 
 resource "random_string" "bucket_name" {
