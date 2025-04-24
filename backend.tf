853d26a2b120a24e573e38853ed2ea9677647098
BruhSlave <braslpaf@gmail.com>
2025-04-24 11:07:03 +0000

    Delete keys init by vars

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
     bucket   = "vkr-terraform-state"
-    key      = "prod.tfstate"                
+    key      = "prod.tfstate"
     endpoint = "https://storage.yandexcloud.net"
     region   = "ru-central1"
-    access_key = var.access_key              
-    secret_key = var.secret_key
   }
 }
